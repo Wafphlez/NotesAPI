9ec72bbcce237606ea90a00f5bfa2c438545e607
Wafphlez <wafphlez@gmail.com>
2022-03-01 15:15:16 +0300

    Initial commit

---
+++ NotesAPI/Models/Note.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NotesAPI.Models
+{
+    public class Note
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
